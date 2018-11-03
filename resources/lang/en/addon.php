befd09287857c3266a77b1335fa30cf0bfe3304d
Ryan Thompson <ryan@pyrocms.com>
2018-11-03 10:59:38 -0500

    Initial commit

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Seo Block',
+    'name'        => 'Seo Block Extension',
+    'description' => ''
+];
---
+++ tests/Feature/SeoBlockExtensionTest.php
@@ -0,0 +1,10 @@
+<?php namespace Anomaly\SeoBlockExtension\Test\Feature;
+
+class SeoBlockExtensionTest extends \TestCase
+{
+
+    public function testFeature()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+}
